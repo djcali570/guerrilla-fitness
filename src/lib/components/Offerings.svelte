2522639f97a9e507281fff082e27c8b7e8521ed9
djcali570 <djcali570@gmail.com>
2025-05-20 22:21:19 -0400

    updated image links

--- src/lib/components/Offerings.svelte
+++ src/lib/components/Offerings.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import ImageCard from "./ImageCard.svelte";
-
+	import { PUBLIC_WEB_IMAGE_ENDPOINT } from '$env/static/public';
+	import ImageCard from './ImageCard.svelte';
 </script>
 
 <h3 class="h3sr">Offerings</h3>
@@ -8,7 +8,7 @@
 	<div class="md:col-start-1 md:row-span-6 h-[430px]">
 		<div class="w-full h-full">
 			<ImageCard
-				imgLink="http://localhost/v1/storage/buckets/web/files/photoa/view?project=guerrilla&project=guerrilla"
+				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photoa/view?project=guerrilla&project=guerrilla"
 				title="CROSSFIT"
 				desc="Unlimited access to 40+ CrossFit classes per week"
 			/>
@@ -17,7 +17,7 @@
 	<div class="md:col-start-2 md:row-span-6 md:row-start-3 md:row-end-9 h-[430px]">
 		<div class="w-full h-full flex justify-center">
 			<ImageCard
-				imgLink="http://localhost/v1/storage/buckets/web/files/photob/view?project=guerrilla&project=guerrilla"
+				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photob/view?project=guerrilla&project=guerrilla"
 				title="NUTRITION"
 				desc="Dial in your nutrition with Nutrition and Accountability coaching"
 			/>
@@ -26,7 +26,7 @@
 	<div class="md:col-start-3 md:row-span-6 md:row-start-5 md:row-end-11 h-[430px]">
 		<div class="w-full h-full flex justify-end">
 			<ImageCard
-				imgLink="http://localhost/v1/storage/buckets/web/files/photoc/view?project=guerrilla&project=guerrilla"
+				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photoc/view?project=guerrilla&project=guerrilla"
 				title="PRIVATE TRAINING"
 				desc="Work towards specific goals with private personal training"
 			/>
