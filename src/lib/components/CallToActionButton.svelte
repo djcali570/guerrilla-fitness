a5c7d13bacaaa806c359e69f2894cdfc1b5cadb1
djcali570 <djcali570@gmail.com>
2025-05-26 09:25:59 -0400

    updated to use as button or a link

--- src/lib/components/CallToActionButton.svelte
+++ src/lib/components/CallToActionButton.svelte
@@ -1,15 +1,58 @@
 <script lang="ts">
-let {link="/", title = 'Title'}:{link?: string; title?: string;} = $props();
+	let {
+		link = '/',
+		title = 'Title',
+		textColor = '#fefffe',
+		bgColor = 'transparent',
+		borderColor = '#bf1e2d',
+		type = 'link',
+		click = () => {}
+	}: {
+		link?: string;
+		title?: string;
+		textColor?: string;
+		bgColor?: string;
+		borderColor?: string;
+		type?: 'link' | 'btn';
+		click?: () => void;
+	} = $props();
 </script>
 
-<a href="{link}" class="button">{title}</a>
+{#if type === 'link'}
+	<a
+		href={link}
+		class="button"
+		style="--bgColor: {bgColor}; --borderColor: {borderColor}; --textColor: {textColor}"
+		><div class="w-full h-full flex justify-center items-center">{title}</div></a
+	>
+{:else}
+	<button
+		onclick={click}
+		class="button"
+		style="--bgColor: {bgColor}; --borderColor: {borderColor}; --textColor: {textColor}"
+	>
+		<div class="w-full h-full flex justify-center items-center">{title}</div>
+	</button>
+{/if}
 
 <style lang="postcss">
 	.button {
-		@apply w-32 h-12;
-		@apply flex items-center justify-center;
-		@apply uppercase text-g-white-500 text-sm font-ibm-plex-regular;
-		@apply border-2 border-g-primary-500;
-        @apply hover:bg-g-primary-500 transition-colors duration-300;
+		display: flex;
+		justify-items: center;
+		align-items: center;
+		width: 8rem;
+		height: 3rem;
+		font-size: 0.875rem;
+		font-family: ibm-plex-regular;
+		color: var(--textColor);
+		background-color: var(--bgColor);
+		border-color: var(--borderColor);
+		border-width: 2px;
+		text-transform: uppercase;
+		transition-duration: 300ms;
+		transition-property: color, background-color, border-color, text-decoration-color, fill, stroke;
+	}
+	.button:hover {
+		filter: brightness(1.2);
 	}
 </style>
