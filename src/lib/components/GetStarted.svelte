b7622a7490d51058905d1b6502532da455cfcfbb
djcali570 <djcali570@gmail.com>
2025-05-26 09:25:41 -0400

    updated to use title as a prop

--- src/lib/components/GetStarted.svelte
+++ src/lib/components/GetStarted.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import CallToActionButton from './CallToActionButton.svelte';
+	let { title = 'Get Started' }: { title?: string } = $props();
 </script>
 
 <div class="w-full flex flex-col">
@@ -7,7 +8,7 @@
 		<h4 class="h4sr" style="letter-spacing: 1pt;">CrossFit. Nutrition. Accountability. Results.</h4>
 	</div>
 	<div class="w-full flex justify-center py-6 md:py-8">
-		<h1 class="video__title__text">Get Started</h1>
+		<h1 class="video__title__text">{title}</h1>
 	</div>
 	<div class="w-full flex justify-center">
 		<CallToActionButton title="3 Class Trial" />
