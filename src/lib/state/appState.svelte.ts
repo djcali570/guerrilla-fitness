e79e1092bd5f7ad24a0680f985cfce39318e08ed
djcali570 <djcali570@gmail.com>
2025-05-26 09:25:02 -0400

    added gym data

--- src/lib/state/appState.svelte.ts
+++ src/lib/state/appState.svelte.ts
@@ -1,4 +1,5 @@
-import type { link } from "$lib/types";
+import { PUBLIC_WEB_IMAGE_ENDPOINT } from "$env/static/public";
+import type { Gym, link } from "$lib/types";
 
 /**
  *  Menu Actions
@@ -26,6 +27,22 @@ export const counter = $state({
 export const links: link[] = [
     { href: '/', title: 'get started', sublinks: [{ href: '/', title: 'Join Now' }, { href: '/', title: 'Nutrition Coaching' }] },
     { href: '/', title: 'Locations', sublinks: [{ href: '/montclair', title: 'Montclair' }, { href: '/paramus', title: 'Paramus' }] },
-    { href: '/meet-the-team', title: 'Meet The Team' },
+    { href: '/meet-the-team', title: 'Meet The Team' },{ href: '/contact', title: 'Conact Us' },
 ];
 
+export const gyms: Gym[] = [
+    {
+        name: 'Montclair',
+        address: '19 Elm St, Montclair NJ 07042',
+        phone: '1-973-337-6538',
+        image: `${PUBLIC_WEB_IMAGE_ENDPOINT}/temp_gym1/view?project=guerrilla&project=guerrilla`,
+        link: '/montclair'
+    },
+    {
+        name: 'Paramus',
+        address: '348 Evelyn St, Paramus NJ 07652',
+        phone: '1-201-967-5000',
+        image: `${PUBLIC_WEB_IMAGE_ENDPOINT}/temp_gym2/view?project=guerrilla&project=guerrilla`,
+        link: '/paramus'
+    }
+];
