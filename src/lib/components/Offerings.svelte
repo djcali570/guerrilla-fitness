95426d49bc22237dc8e5bd7e9d8d8927ce337029
djcali570 <djcali570@gmail.com>
2025-07-16 12:28:35 -0400

    added animations

--- src/lib/components/Offerings.svelte
+++ src/lib/components/Offerings.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import { PUBLIC_WEB_IMAGE_ENDPOINT } from '$env/static/public';
+	import { slideIn } from '$lib/gsapFunc';
 	import ImageCard from './ImageCard.svelte';
 </script>
 
 <h3 class="h3sr">Offerings</h3>
 <div class="grid grid-rows-3 gap-3 md:gap-0 md:grid-cols-3 md:grid-rows-12">
 	<div class="md:col-start-1 md:row-span-6 h-[430px]">
-		<div class="w-full h-full">
+		<div class="w-full h-full" {@attach slideIn}>
 			<ImageCard
 				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photoa/view?project=guerrilla&project=guerrilla"
 				title="CROSSFIT"
@@ -15,7 +16,7 @@
 		</div>
 	</div>
 	<div class="md:col-start-2 md:row-span-6 md:row-start-3 md:row-end-9 h-[430px]">
-		<div class="w-full h-full flex justify-center">
+		<div class="w-full h-full flex justify-center" {@attach (node)=> slideIn(node, 0.2)}>
 			<ImageCard
 				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photob/view?project=guerrilla&project=guerrilla"
 				title="NUTRITION"
@@ -24,7 +25,7 @@
 		</div>
 	</div>
 	<div class="md:col-start-3 md:row-span-6 md:row-start-5 md:row-end-11 h-[430px]">
-		<div class="w-full h-full flex justify-end">
+		<div class="w-full h-full flex justify-end" {@attach (node)=> slideIn(node, 0.5)}>
 			<ImageCard
 				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photoc/view?project=guerrilla&project=guerrilla"
 				title="PRIVATE TRAINING"
