35fe2b6e2d9166ad07a81d7b6f41f59400a6ed7e
djcali570 <djcali570@gmail.com>
2025-07-16 12:28:26 -0400

    added animations

--- src/lib/components/GetStarted.svelte
+++ src/lib/components/GetStarted.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { slideIn } from '$lib/gsapFunc';
 	import CallToActionButton from './CallToActionButton.svelte';
 	let { title = 'Get Started' }: { title?: string } = $props();
 </script>
@@ -8,9 +9,9 @@
 		<h4 class="h4sr" style="letter-spacing: 1pt;">CrossFit. Nutrition. Accountability. Results.</h4>
 	</div>
 	<div class="w-full flex justify-center py-6 md:py-8">
-		<h1 class="video__title__text">{title}</h1>
+		<h1 class="video__title__text" {@attach (node) => slideIn(node, 0.4)}>{title}</h1>
 	</div>
 	<div class="w-full flex justify-center">
-		<CallToActionButton title="3 Class Trial" />
+		<CallToActionButton title="3 Class Trial"  useSlideIn={true}/>
 	</div>
 </div>
