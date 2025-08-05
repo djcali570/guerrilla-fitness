28bb8b3d7b6ca2a78bdbf810138514f78bcbc459
djcali570 <djcali570@gmail.com>
2025-08-05 09:41:08 -0400

    updated layout to line up with top section

--- src/routes/meet-the-team/+page.svelte
+++ src/routes/meet-the-team/+page.svelte
@@ -6,7 +6,7 @@
 	import Nav from '$lib/components/Nav.svelte';
 	import { slideIn } from '$lib/gsapFunc';
 	import { coaches } from '$lib/state/appState.svelte';
-	import { acs, type Coach } from '$lib/types';
+	import { acs } from '$lib/types';
 	import { Accordion5 } from '@djcali570/component-lib';
 
 	const contentImage = `${PUBLIC_WEB_IMAGE_ENDPOINT}/6709df20c5ef3b5c29c0/view?project=${PUBLIC_PROJECT_ID}`;
@@ -17,36 +17,36 @@
 	<meta name="description" content="About our coaches" />
 	<meta property="og:title" content="Guerrilla Fitness - Meet The Team" />
 	<meta property="og:description" content="About our coaches" />
-	<meta property="og:image" content="{contentImage}" />
+	<meta property="og:image" content={contentImage} />
 	<meta name="twitter:card" content="{contentImage}}" />
 </svelte:head>
 
 <Nav />
 
 <main class="nav__margin bg-g-white-500">
-	<section class="bg-g-black-500 p-8 md:px-24" title="Title">
-		<div class="grid md:grid-cols-2 h-[200px] gap-8">
-			<div class="w-full flex justify-start items-center">
-				<h1 class="h1big">meet the team</h1>
-			</div>			
-			<div class="flex justify-end items-center">
+	<section class="bg-g-black-500 p-8 flex justify-center" title="Title">
+		<div class="w-[1400px] grid md:grid-cols-2 h-[200px] md:gap-8">
+			<div class="w-full flex justify-center md:justify-start items-center">
+				<h1 class="h_sf_b_l_u text-[3rem] md:text-[4.2rem] tracking-wider">meet the team</h1>
+			</div>
+			<div class="flex justify-center md:justify-end items-center">
 				<div class="pr-2"><CallToActionButton title="Join Now" /></div>
 				<div class="pl-2"><CallToActionButton title="Contact" link="/contact" /></div>
 			</div>
 		</div>
 	</section>
-	<section title="Team Grid">
-		<div class="flex flex-col md:grid md:grid-cols-3 w-full gap-8 p-8 md:px-24">
+	<section class="flex justify-center" title="Team Grid">
+		<div class="flex flex-col md:grid md:grid-cols-3 p-8 md:p-0 md:pt-8 gap-8 w-[1400px]">
 			{#each coaches as c}
-				<div class="h-full flex flex-col" {@attach slideIn}>
+				<div class="w-full h-full flex flex-col" {@attach slideIn}>
 					<div class="w-full h-full fca flex-col">
 						<img
-							class="w-full h-auto object-cover aspect-[22/22] max-w-[350px] md:max-w-[400px] object-top"
+							class="w-full h-auto object-cover aspect-[22/22] object-top"
 							src={c.img}
 							alt={c.name}
 						/>
 
-						<div class="pt-4 w-full max-w-[350px] md:max-w-[400px]">
+						<div class="pt-4 w-full">
 							<div class="h_sf_r_d_u text-[3rem]">{c.name}</div>
 							<Accordion5 colorScheme={acs}>
 								{#snippet title()}
