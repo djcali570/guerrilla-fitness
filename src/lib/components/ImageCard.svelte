ef47a7c3f66bd096ee0b101de3d97adf4a033510
djcali570 <djcali570@gmail.com>
2025-08-07 13:02:06 -0400

    updated title text location

--- src/lib/components/ImageCard.svelte
+++ src/lib/components/ImageCard.svelte
@@ -4,7 +4,7 @@
 
 <div class="w-full h-full relative max-w-full md:max-w-[350px]">
 	<img src={imgLink} alt="card" class="w-full h-full object-cover" />
-	<div class="w-full flex flex-col justify-center absolute bottom-[10%] px-2">
+	<div class="w-full flex flex-col justify-center absolute bottom-[8%] px-2">
 		<h1 class=" text-g-white-500 s__font__b text__title text-center leading-[3rem]">{title}</h1>
 		<div class="md:hidden bg-black w-full flex justify-center py-4">
 			<p class="prw text-center">{desc}</p>
