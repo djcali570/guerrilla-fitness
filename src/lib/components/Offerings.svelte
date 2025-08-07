33abe41beaa2145939e8ff33883093279deaa297
djcali570 <djcali570@gmail.com>
2025-08-07 13:02:17 -0400

    updated for tablet

--- src/lib/components/Offerings.svelte
+++ src/lib/components/Offerings.svelte
@@ -5,32 +5,38 @@
 </script>
 
 <h3 class="h3sr">Programs</h3>
-<div class="grid grid-rows-3 gap-3 md:gap-0 md:grid-cols-3 md:grid-rows-12">
-	<div class="md:col-start-1 md:row-span-6 h-[430px]">
-		<div class="w-full h-full" {@attach slideIn}>
-			<ImageCard
-				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photoa/view?project=guerrilla&project=guerrilla"
-				title="CROSSFIT"
-				desc="Unlimited access to classes 7 days/wk"
-			/>
+<div class="w-full flex justify-center mt-10">
+	<div class="grid grid-rows-3 gap-4 lg:gap-10 md:grid-cols-3 md:grid-rows-12 w-full lg:w-[85%]">
+		<div class="md:col-start-1 md:row-span-6 h-[430px] md:h-[300px] lg:h-[430px]">
+			<div class="w-full h-full" {@attach slideIn}>
+				<ImageCard
+					imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photoa/view?project=guerrilla&project=guerrilla"
+					title="CROSSFIT"
+					desc="Unlimited access to classes 7 days/wk"
+				/>
+			</div>
 		</div>
-	</div>
-	<div class="md:col-start-2 md:row-span-6 md:row-start-3 md:row-end-9 h-[430px]">
-		<div class="w-full h-full flex justify-center" {@attach (node)=> slideIn(node, 0.2)}>
-			<ImageCard
-				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photob/view?project=guerrilla&project=guerrilla"
-				title="NUTRITION"
-				desc="Optimize your health with Nutrition and accountability coaching"
-			/>
+		<div
+			class="md:col-start-2 md:row-span-6 md:row-start-3 md:row-end-9 h-[430px] md:h-[300px] lg:h-[430px]"
+		>
+			<div class="w-full h-full flex justify-center" {@attach (node) => slideIn(node, 0.2)}>
+				<ImageCard
+					imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photob/view?project=guerrilla&project=guerrilla"
+					title="NUTRITION"
+					desc="Optimize your health with Nutrition and accountability coaching"
+				/>
+			</div>
 		</div>
-	</div>
-	<div class="md:col-start-3 md:row-span-6 md:row-start-5 md:row-end-11 h-[430px]">
-		<div class="w-full h-full flex justify-end" {@attach (node)=> slideIn(node, 0.5)}>
-			<ImageCard
-				imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photoc/view?project=guerrilla&project=guerrilla"
-				title="PRIVATE/SEMI PRIVATE TRAINING"
-				desc="Pursue your goals with focused 1-on-1 or small group coaching sessions"
-			/>
+		<div
+			class="md:col-start-3 md:row-span-6 md:row-start-5 md:row-end-11 h-[430px] md:h-[300px] lg:h-[430px]"
+		>
+			<div class="w-full h-full flex justify-end" {@attach (node) => slideIn(node, 0.5)}>
+				<ImageCard
+					imgLink="{PUBLIC_WEB_IMAGE_ENDPOINT}/photoc/view?project=guerrilla&project=guerrilla"
+					title="PRIVATE/SEMI PRIVATE TRAINING"
+					desc="Pursue your goals with focused 1-on-1 or small group coaching sessions"
+				/>
+			</div>
 		</div>
 	</div>
 </div>
